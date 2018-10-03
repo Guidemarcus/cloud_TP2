7a14b9bdd78de31a28f51231601a5ece5ea14036
llDrLove <oliver.love.34@gmail.com>
2018-10-03 16:41:29 -0400

    run the load_balancer locally & add the build files

---
+++ src/loadBalancer/LoadBalancer.java
@@ -0,0 +1,47 @@
+package loadBalancer;
+
+import java.rmi.ConnectException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import shared.LoadBalancerInterface;
+
+public class LoadBalancer implements LoadBalancerInterface {
+	
+	public static void main(String[] args) {
+		LoadBalancer loadBalancer = new LoadBalancer();
+		loadBalancer.run();
+	}
+
+	public LoadBalancer() {
+		super();
+	}
+
+	private void run() {
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+		try {
+			LoadBalancerInterface stub = (LoadBalancerInterface) UnicastRemoteObject
+					.exportObject(this, 0);
+			Registry registry = LocateRegistry.createRegistry(5001);
+			registry.bind("load_balancer", stub);
+		} catch (ConnectException e) {
+			System.err.println("Impossible de se connecter au registre RMI. Est-ce que rmiregistry est lancé ?");
+			System.err.println();
+			System.err.println("Erreur: " + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("Erreur: " + e.getMessage());
+		}
+	}
+}
+
---
+++ src/shared/LoadBalancerInterface.java
@@ -0,0 +1,7 @@
+package shared;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface LoadBalancerInterface extends Remote {
+}
